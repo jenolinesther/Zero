021443904b40a941b9b585a649ddd41baaad8767
jenolinesther <jenolinesther31@gmail.com>
2025-02-10 11:04:41 +0530

    first commit

---
+++ src/main/java/com/quiz/quiz/quizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class quizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(quizApplication.class, args);
+	}
+
+}
